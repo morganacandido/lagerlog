1a79885b5c3746d8349c85df016bd17008b03fdf
Morgana Candido <95589687+morganacandido@users.noreply.github.com>
2024-08-22 13:49:17 -0300

    Create Relatorio.java

---
+++ model/Relatorio.java
@@ -0,0 +1,11 @@
+package com.maltepuro.lagerlog.model;
+
+import com.maltepuro.lagerlog.controller.RelatorioController;
+
+public class Relatorio{
+    public static void main(String[] args) {
+        RelatorioController relatorio= new RelatorioController();
+        relatorio.geraRelatorio();
+      
+    }
+}
